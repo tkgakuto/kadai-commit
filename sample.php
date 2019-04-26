9605b1cf0bf34853442a20fd8c117483462c1784
tkgakuto <s20ah198@seinan-gakuin.jp>
2019-04-26 12:37:34 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <head>
+            <h1>おみくじ</h1>
+            <p>おみくじの結果:なし</p>
+            <form action="sample.php" method="POST">
+                <button type="submit" name="act" value="draw"></button>
+            </form>
+        </head>
+    </body>
+</html>
